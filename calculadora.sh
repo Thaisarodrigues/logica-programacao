cc3440f14ba6cf0c014c05916e6380760930620b
Thaisarodrigues <gr.thaisa@gmail.com>
2025-06-03 20:18:42 -0300

    Create calculadora.sh
    Create file calculadora

---
+++ calculadora.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo Calculadora
+
+echo SOMA
+echo x = 1
+echo y = 4
+echo x + y 
+soma = input('5')
+print('soma')
+
+echo SUBTRAÇÃO
+echo x - y
+subtração = input('- 3')
+print = ('subtração')
+
+
+echo MULTIPLICAÇÃO
+echo x * y
+multiplicação = input('4')
+print = ('multiplicação')
+
+echo MULTIPLICAÇÃO
+echo x * y
+multiplicação = input('4')
+print = ('multiplicação')
+
+echo POTENCIAÇÃO
+echo x ** y
+potenciação = input('1')
+print = ('potenciação')
+
